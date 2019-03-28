258e3dcf7404ef08026d9a306692aff260457494
wspoons5 <michael.wotherspoon@gmail.com>
2019-03-27 19:10:54 -0700

    Updated stats.r file.

--- stats.r
+++ stats.r
@@ -1,4 +1,5 @@
 library("ggplot2")
+library("gridExtra")
 
 setwd("C:/Users/Michael/Projects/ThreadOptimizer")
 
@@ -6,33 +7,11 @@ compDat = read.csv("compThreadTestMaster.csv", header=T)
 urlDat = read.csv("urlThreadTestMaster.csv", header=T)
 rwDat = read.csv("readWriteThreadTestMaster.csv", header=T)
 
-ggplot(urlDat, aes(threadCount, runTime)) + geom_point() + geom_smooth()
-ggplot(compDat, aes(threadCount, runTime)) + geom_point() + geom_smooth()
-ggplot(rwDat, aes(threadCount, runTime)) + geom_point() + geom_smooth()
+p1 = ggplot(compDat, aes(threadCount, runTime), main) + geom_point() + 
+       geom_smooth() + ggtitle("Computationally Intensive Load")
+p2 = ggplot(urlDat, aes(threadCount, runTime)) + geom_point() + 
+       geom_smooth() + ggtitle("Url Request Intensive Load")
+p3 = ggplot(rwDat, aes(threadCount, runTime)) + geom_point() +
+       geom_smooth() + ggtitle("Input/Output Intensive Load")
 
-dat1 = dat[ which(dat$threadCount==1 | dat$threadCount==2), ]
-dat1$threadCount = as.factor((dat1$threadCount))
-
-dat2 = dat[ which(dat$threadCount==2 | dat$threadCount==3), ]
-dat2$threadCount = as.factor((dat2$threadCount))
-
-dat3 = dat[ which(dat$threadCount==3 | dat$threadCount==4), ]
-dat3$threadCount = as.factor((dat3$threadCount))
-
-dat4 = dat[ which(dat$threadCount==4 | dat$threadCount==5), ]
-dat4$threadCount = as.factor((dat4$threadCount))
-
-dat5 = dat[ which(dat$threadCount==5 | dat$threadCount==6), ]
-dat5$threadCount = as.factor((dat5$threadCount))
-
-m1 = glm(threadCount ~ runTime, data = dat1, family = "binomial")
-m2 = glm(threadCount ~ runTime, data = dat2, family = "binomial")
-m3 = glm(threadCount ~ runTime, data = dat3, family = "binomial")
-m4 = glm(threadCount ~ runTime, data = dat4, family = "binomial")
-m5 = glm(threadCount ~ runTime, data = dat5, family = "binomial")
-
-summary(m1)
-summary(m2)
-summary(m3)
-summary(m4)
-summary(m5)
+grid.arrange(p1, p2, p3, ncol=2)
